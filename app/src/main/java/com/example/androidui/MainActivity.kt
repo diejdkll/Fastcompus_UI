c97bb3e60e61ba960c3bf7fe92496a29b00c551b
hyeonseo0412 <s22026@gsm.hs.kr>
2022-11-16 21:13:35 +0900

    test

--- app/src/main/java/com/example/androidui/MainActivity.kt
+++ app/src/main/java/com/example/androidui/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.androidui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Lifecycle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.homework_1)
+        setContentView(R.layout.activity_main)
+
+        Log.d("Lifecycle", "onCreate")
+    }
+
+    override fun onStart() {
+        Log.d("Lifecycle", "onStart")
+
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("Lifecycle", "onResume")
+
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("Lifecycle", "onPause")
+
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("Lifecycle", "onStop")
+
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("Lifecycle", "onDestroy")
+
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d("Lifecycle", "onRestart")
+
+        super.onRestart()
     }
 }
