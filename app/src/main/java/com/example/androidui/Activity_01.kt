e2047450c11771e15527999cd1d5b13900566b95
hyeonseo0412 <s22026@gsm.hs.kr>
2022-11-19 23:38:12 +0900

    test

---
+++ app/src/main/java/com/example/androidui/Activity_01.kt
@@ -0,0 +1,46 @@
+package com.example.androidui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class Activity_01 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_01)
+
+        Log.d("LifeCycle", "onCreate")
+
+    }
+
+    override fun onResume() {
+        Log.d("LifeCycle", "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("LifeCycle", "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("LifeCycle", "onStop")
+        super.onStop()
+    }
+
+
+    override fun onStart() {
+        Log.d("LifeCycle", "onStart")
+        super.onStart()
+    }
+
+    override fun onDestroy() {
+        Log.d("LifeCycle", "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d("LifeCycle", "onRestart")
+        super.onRestart()
+    }
+}
--- app/src/main/java/com/example/androidui/MainActivity.kt
+++ app/src/main/java/com/example/androidui/MainActivity.kt
@@ -9,43 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Log.d("Lifecycle", "onCreate")
-    }
-
-    override fun onStart() {
-        Log.d("Lifecycle", "onStart")
-
-        super.onStart()
-    }
-
-    override fun onResume() {
-        Log.d("Lifecycle", "onResume")
-
-        super.onResume()
-    }
-
-    override fun onPause() {
-        Log.d("Lifecycle", "onPause")
-
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d("Lifecycle", "onStop")
-
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.d("Lifecycle", "onDestroy")
-
-        super.onDestroy()
-    }
-
-    override fun onRestart() {
-        Log.d("Lifecycle", "onRestart")
-
-        super.onRestart()
     }
 }
