b6708367a175ead419a3b954884c23df9c9559ee
hyeonseo0412 <s22026@gsm.hs.kr>
2022-11-29 21:00:34 +0900

    test

---
+++ app/src/main/java/com/example/androidui/ActivityThree.kt
@@ -0,0 +1,24 @@
+package com.example.androidui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ActivityThree : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_three)
+
+        (findViewById<TextView>(R.id.one)).setOnClickListener{
+            startActivity(Intent(this@ActivityThree, ActivityOne::class.java))
+        }
+        (findViewById<TextView>(R.id.two)).setOnClickListener{
+            startActivity(Intent(this@ActivityThree, ActivityTwo::class.java))
+        }
+        (findViewById<TextView>(R.id.three)).setOnClickListener{
+            startActivity(Intent(this@ActivityThree, ActivityThree::class.java))
+        }
+    }
+
+}
