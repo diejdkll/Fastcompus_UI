af8c5ec712501e49973df9a123fa55e23eca9cd5
hyeonseo0412 <s22026@gsm.hs.kr>
2022-12-15 17:07:15 +0900

    Pager에 FragmentPagerAdepter 만들고 장착하기 tabLayout과 Pager연결하기

--- app/src/main/java/com/example/androidui/TablayoutPagerActivity.kt
+++ app/src/main/java/com/example/androidui/TablayoutPagerActivity.kt
@@ -2,6 +2,9 @@ package com.example.androidui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 
@@ -13,9 +16,42 @@ class TablayoutPagerActivity : AppCompatActivity() {
         val tabLayout = findViewById<TabLayout>(R.id.taplayout)
         val viewPager = findViewById<ViewPager>(R.id.viewPager)
 
+        // 탭레이아웃에 탭을 추가하는 방법
         tabLayout.addTab(tabLayout.newTab().setText("1번째"))
         tabLayout.addTab(tabLayout.newTab().setText("2번째"))
         tabLayout.addTab(tabLayout.newTab().setText("3번째"))
 
+        // Pager에 adepter를 장착하는 방법
+        viewPager.adapter = FragmentPagerAdepter(supportFragmentManager, 3)
+
+        // tabLayout과 Pager연결하기
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                viewPager.setCurrentItem(tab!!.position)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+    }
+}
+
+class FragmentPagerAdepter(
+    fragmentManager: FragmentManager,
+    val tabCount: Int
+): FragmentStatePagerAdapter(fragmentManager){
+    override fun getCount(): Int {
+        return  tabCount
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when(position){
+            0 -> return FragmentFirst()
+            1 -> return FragmentFirst()
+            else -> return FragmentFirst()
+        }
     }
 }
