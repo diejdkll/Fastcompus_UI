8e8c7471d26a23fd524c494187d575c054112585
hyeonseo0412 <s22026@gsm.hs.kr>
2023-01-11 19:56:16 +0900

    Room 실습 2편

--- app/src/main/java/com/example/androidui/RoomActivity.kt
+++ app/src/main/java/com/example/androidui/RoomActivity.kt
@@ -2,15 +2,33 @@ package com.example.androidui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
+import androidx.room.OnConflictStrategy.REPLACE
 
 class RoomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room)
     }
+}
+
+@Database(entities = [UserProfile::class], version = 1)
+abstract class UserDatabase : RoomDatabase() {
+    abstract fun userProfileDao(): UserPorfileDao
+}
+
+@Dao
+interface UserPorfileDao {
+    // CRUD -> 데이터 베이스 조작
+    // Query -> 데이터 베이스 조회
+    @Insert(onConflict = REPLACE)
+    fun insert(userProfile: UserProfile)
+
+    @Query("DELETE FROM userprofile WHERE id = :userId")
+    fun delete(userId: Int)
+
+    @Query("SELECT * FROM userprofile")
+    fun getAll(): List<UserProfile>
 }
 
 @Entity
