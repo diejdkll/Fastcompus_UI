327fa5d598e1fa926f54a594eb7a08e78fa60b87
hyeonseo0412 <s22026@gsm.hs.kr>
2023-01-12 20:36:46 +0900

    Room 실습 3편

--- app/src/main/java/com/example/androidui/RoomActivity.kt
+++ app/src/main/java/com/example/androidui/RoomActivity.kt
@@ -2,23 +2,53 @@ package com.example.androidui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.DocumentsContract.Root
+import android.util.Log
 import androidx.room.*
 import androidx.room.OnConflictStrategy.REPLACE
+import com.example.androidui.databinding.ActivityRoomBinding
 
 class RoomActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRoomBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRoomBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_room)
+        setContentView(binding.root)
+
+        // 기본적으로 데이터베이스 작업은 메인 쓰레드에서 할 수 없다
+        // 이유는, 데이터베이스 작업을 휴대폰이 하는 동안 사용자가 기다려야하기때문
+        // 해결책은, 쓰레드를 이용하거나 async를 이용하면 된다
+        val database = Room.databaseBuilder(
+            applicationContext,
+            UserDatabase::class.java,
+            "user-database"
+        ).allowMainThreadQueries().build()
+
+        binding.save.setOnClickListener {
+            val userProfile = UserProfile("홍", "길동")
+            database.userProfileDao().insert(userProfile)
+        }
+
+        binding.load.setOnClickListener {
+            val userProfiles = database.userProfileDao().getAll()
+            userProfiles.forEach {
+                Log.d("testt", "" + it.id + it.firstName)
+            }
+        }
+
+        binding.delete.setOnClickListener {
+            database.userProfileDao().delete(1)
+        }
     }
 }
 
 @Database(entities = [UserProfile::class], version = 1)
 abstract class UserDatabase : RoomDatabase() {
-    abstract fun userProfileDao(): UserPorfileDao
+    abstract fun userProfileDao(): UserProfileDao
 }
 
 @Dao
-interface UserPorfileDao {
+interface UserProfileDao {
     // CRUD -> 데이터 베이스 조작
     // Query -> 데이터 베이스 조회
     @Insert(onConflict = REPLACE)
@@ -38,4 +68,6 @@ class UserProfile(
     val lastName: String,
     @ColumnInfo(name = "first_name")
     val firstName: String
-)
+) {
+    constructor(lastName: String, firstName: String) : this(0, lastName, firstName)
+}
