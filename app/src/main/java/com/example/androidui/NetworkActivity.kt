cae510db00ceb51b2c7439fef68362324a62db44
hyeonseo0412 <s22026@gsm.hs.kr>
2023-01-20 12:23:34 +0900

    Network 실습 1편

---
+++ app/src/main/java/com/example/androidui/NetworkActivity.kt
@@ -0,0 +1,33 @@
+package com.example.androidui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class NetworkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_network)
+
+        val urlString: String = "https://mellowcode.org/json/students/"
+        val url: URL = URL(urlString)
+        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+        connection.requestMethod = "GET"
+        connection.setRequestProperty("Content-Type", "application/json")
+
+        var buffer = ""
+        if(connection.responseCode == HttpURLConnection.HTTP_OK){
+            val reader = BufferedReader(
+                InputStreamReader(
+                    connection.inputStream,
+                    "UTF-8"
+                )
+            )
+            buffer = reader.readLine()
+        }
+    }
+}
