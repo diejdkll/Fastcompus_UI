3b94d59bea3bd38c4d80db35d367e34c96c87915
hyeonseo0412 <s22026@gsm.hs.kr>
2023-01-24 21:51:50 +0900

    Network 실습 2편

--- app/src/main/java/com/example/androidui/NetworkActivity.kt
+++ app/src/main/java/com/example/androidui/NetworkActivity.kt
@@ -1,7 +1,9 @@
 package com.example.androidui
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -12,7 +14,13 @@ class NetworkActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_network)
 
-        val urlString: String = "https://mellowcode.org/json/students/"
+        NetworkAsyncTask().execute()
+    }
+}
+
+class NetworkAsyncTask() : AsyncTask<Any?, Any?, Any?>() {
+    override fun doInBackground(vararg params: Any?): Any? {
+        val urlString: String = "http://mellowcode.org/json/students/"
         val url: URL = URL(urlString)
         val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
 
@@ -20,7 +28,7 @@ class NetworkActivity : AppCompatActivity() {
         connection.setRequestProperty("Content-Type", "application/json")
 
         var buffer = ""
-        if(connection.responseCode == HttpURLConnection.HTTP_OK){
+        if (connection.responseCode == HttpURLConnection.HTTP_OK) {
             val reader = BufferedReader(
                 InputStreamReader(
                     connection.inputStream,
@@ -28,6 +36,8 @@ class NetworkActivity : AppCompatActivity() {
                 )
             )
             buffer = reader.readLine()
+            Log.d("testt", buffer)
         }
+        return null
     }
 }
