06a720832757d789f14f49b2c6b9cdb3d02e7477
hyeonseo0412 <s22026@gsm.hs.kr>
2023-01-29 22:48:21 +0900

    Network 실습 4편

--- app/src/main/java/com/example/androidui/RetrofitActivity.kt
+++ app/src/main/java/com/example/androidui/RetrofitActivity.kt
@@ -2,14 +2,41 @@ package com.example.androidui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retrofit)
 
+        // 서버 -> 읽을 수 없는 데이터 -> JSON -> Gson
+        // GSON -> 읽을 수 없는 데이터를 코틀린 객체로 바꿔준다
         val retrofit = Retrofit.Builder()
             .baseUrl("http://mellowcode.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val retrofitService = retrofit.create(RetrofitService::class.java)
+
+        retrofitService.getStudentList().enqueue(object : Callback<ArrayList<StudentFromServer>>{
+            override fun onResponse(
+                call: Call<ArrayList<StudentFromServer>>,
+                response: Response<ArrayList<StudentFromServer>>
+            ) {
+                if(response.isSuccessful){
+                    val studentList = response.body()
+                    studentList!!.forEach {
+                        Log.d("testt", ""+it.name)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<StudentFromServer>>, t: Throwable) {
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/androidui/RetrofitService.kt
@@ -0,0 +1,13 @@
+package com.example.androidui
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+class StudentFromServer(
+    val id: Int, val name: String, val age: Int, val intro: String
+)
+
+interface RetrofitService {
+    @GET("json/students")
+    fun getStudentList(): Call<ArrayList<StudentFromServer>>
+}
